706429a530d8fa0cc3f1f5833238cd73dd33b21c
hannaf <hanna.figueiredo@elo7.com>
2016-06-26 16:44:11 -0300

    Corrige Converter de Data

--- src/main/java/br/com/homecare/model/OffsetDateTimePersistenceConverter.java
+++ src/main/java/br/com/homecare/model/OffsetDateTimePersistenceConverter.java
@@ -1,23 +1,23 @@
 package br.com.homecare.model;
 
-import java.sql.Timestamp;
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
+import java.util.Date;
 
 import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
 
 @Converter(autoApply = true)
-public class OffsetDateTimePersistenceConverter implements AttributeConverter<OffsetDateTime, String> {
+public class OffsetDateTimePersistenceConverter implements AttributeConverter<OffsetDateTime, Date> {
 
 	@Override
-	public String convertToDatabaseColumn(OffsetDateTime entityValue) {
-		return Timestamp.valueOf(entityValue.atZoneSameInstant(ZoneOffset.UTC).toLocalDateTime()).toString();
+	public Date convertToDatabaseColumn(OffsetDateTime entityValue) {
+		return Date.from(entityValue.toInstant());
 	}
 
 	@Override
-	public OffsetDateTime convertToEntityAttribute(String databaseValue) {
-		return OffsetDateTime.parse(databaseValue);
+	public OffsetDateTime convertToEntityAttribute(Date databaseValue) {
+		return OffsetDateTime.ofInstant(databaseValue.toInstant(), ZoneOffset.UTC);
 	}
 
 }
